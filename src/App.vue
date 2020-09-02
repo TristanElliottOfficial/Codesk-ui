1b7550cef389751fa729e536a7b6db6beb0ab7a6
Tristan Elliott <dev@tristanelliott.co.za>
2020-09-03 00:49:21 +0200

    Main App HTML Layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
-  <div id="app" class="bg-blue-700">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div>
+      <aside>
+        <!--Sidebar User Profile Start-->
+        <div>
+          <img src="http://tristanelliott.co.za/development/users/User1.jpg" alt="Users Profile Picture">
+          <h2>Tristan Elliott</h2>
+          <p>Developer</p>
+        </div>
+        <!--Sidebar User Profile End-->
+        <!--Menu Links Start-->
+        <h4>General</h4>
+        <div>
+          <a href="#">Home</a>
+          <a href="#">Friends</a>
+          <a href="#">Messages</a>
+          <a href="#">Notifications</a>
+          <a href="#">Project Invites</a>
+        </div>
+        <h4>Settings</h4>
+        <div>
+          <a href="#">General Settings</a>
+          <a href="#">Profile Settings</a>
+          <a href="#">Privacy Settings</a>
+          <a href="#">Social Settings</a>
+        </div>
+        <!--Menu Links End-->
+      </aside>
+      <div>
+        <header>
+          <!--Main Top Navigation Start-->
+            <nav>
+              <!--Search Input Start-->
+              <div>
+                <form>
+                  <div>
+                    <label for="search">Search Here</label>
+                    <input type="text" name="search" id="search" placeholder="Search ...">
+                  </div>
+                </form>
+              </div>
+              <!--Search Input End-->
+              <!--Account Information Start-->
+              <div class="nav-dropdown">
+                <span class="dropdown-button">Username</span>
+                <ul>
+                  <li><a href="#">Profile</a></li>
+                  <li><a href="#">Friends</a></li>
+                  <li><a href="#">Messages</a></li>
+                  <li><a href="#">Logout</a></li>
+                </ul>
+              </div>
+              <!--Account Information End-->
+            </nav>
+          <!--Main Top Navigation End-->
+        </header>
+        <main>
+        <!--Main Content Start-->
+          <h1>Main Content</h1>
+        <!--Main Content End-->
+        </main>
+        <footer>
+        <!--Main Footer Start-->
+          <p>All rights reserved <a href="http://tristanelliott.co.za">Tristan Elliott</a></p>
+        <!--Main Footer End-->
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // Component Import
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
